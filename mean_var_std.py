7b8adc0dc31538002cc9aef1f409e7041d78b22d
sarthak-geek <sarthakjaronde@gmail.com>
2025-10-19 12:52:48 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,21 +2,15 @@ import numpy as np
 
 def calculate(list):
     if len(list) != 9:
-        raise ValueError("List must contain nine numbers.")
-    a = np.array(arr)
-    a=a.reshape(3,3)
+        raise ValueError("List must contain nine numbers." )
+    a = np.array(list)
+    a = a.reshape(3,3)
     result = {
-    'mean': [],
-    'variance': [],
-    'standard deviation': [],
-    'max': [],
-    'min': [],
-    'sum': []
+        'mean': [a.mean(axis=0).tolist(), a.mean(axis=1).tolist(), float(a.mean())],
+        'variance': [a.var(axis=0).tolist(), a.var(axis=1).tolist(), float(a.var())],
+        'standard deviation': [a.std(axis=0).tolist(), a.std(axis=1).tolist(), float(a.std())],
+        'max': [a.max(axis=0).tolist(), a.max(axis=1).tolist(), float(a.max())],
+        'min': [a.min(axis=0).tolist(), a.min(axis=1).tolist(), float(a.min())],
+        'sum': [a.sum(axis=0).tolist(), a.sum(axis=1).tolist(), float(a.sum())]
     }
-    result["mean"] = [a.mean(axis=0).tolist(),a.mean(axis=0).tolist(),a.mean().tolist()]
-    result["variance"] = [a.var(axis=0).tolist(),a.var(axis=1).tolist(),a.var().tolist()]
-    result["standard deviation"] = [a.std(axis=0).tolist(),a.std(axis=1).tolist(),a.std().tolist()]
-    result["max"] = [a.max(axis=0).tolist(),a.max(axis=1).tolist(),a.max().tolist()]
-    result["min"] = [a.min(axis=0).tolist(),a.min(axis=1).tolist(),a.min().tolist()]
-    result["sum"] = [a.sum(axis=0).tolist(),a.sum(axis=1).tolist(),a.sum().tolist()]
     return result
